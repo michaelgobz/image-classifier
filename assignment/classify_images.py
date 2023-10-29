83dcfe38c7f2cbbfa496e5540099120be315eec5
Michael Goboola <47690301+michaelgobz@users.noreply.github.com>
2023-10-29 17:20:05 +0300

    add the accommodation for multiple classifications

--- assignment/classify_images.py
+++ assignment/classify_images.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/classify_images.py
-#                                                                             
+#
 # PROGRAMMER: Michael Goboola
 # DATE CREATED: 2023-10-26
-# REVISED DATE: 
-# PURPOSE: Create a function classify_images that uses the classifier function 
-#          to create the classifier labels and then compares the classifier 
+# REVISED DATE: 2023-10-29
+# PURPOSE: Create a function classify_images that uses the classifier function
+#          to create the classifier labels and then compares the classifier
 #          labels to the pet image labels. This function inputs:
-#            -The Image Folder as image_dir within classify_images and function 
-#             and as in_arg.dir for function call within main. 
-#            -The results dictionary as results_dic within classify_images 
-#             function and results for the functin call within main.
-#            -The CNN model architecture as model wihtin classify_images function
-#             and in_arg.arch for the function call within main. 
-#           This function uses the extend function to add items to the list 
+#            -The Image Folder as image_dir within classify_images and function
+#             and as in_arg.dir for function call within main.
+#            -The results dictionary as results_dic within classify_images
+#             function and results for the function call within main.
+#            -The CNN model architecture as model within classify_images function
+#             and in_arg.arch for the function call within main.
+#           This function uses the extend function to add items to the list
 #           that's the 'value' of the results dictionary. You will be adding the
-#           classifier label as the item at index 1 of the list and the comparison 
+#           classifier label as the item at index 1 of the list and the comparison
 #           of the pet and classifier labels as the item at index 2 of the list.
 #
 ##
 # Imports classifier function for using CNN to classify images
 from os import listdir, path
-from classifier import classifier 
+from classifier import classifier
 
 # TODO 3: Define classify_images function below, specifically replace the None
-#       below by the function definition of the classify_images function. 
-#       Notice that this function doesn't return anything because the 
-#       results_dic dictionary that is passed into the function is a mutable 
+#       below by the function definition of the classify_images function.
+#       Notice that this function doesn't return anything because the
+#       results_dic dictionary that is passed into the function is a mutable
 #       data type so no return is needed.
-# 
+#
+
+
 def classify_images(images_dir, results_dic, model):
     """
     Creates classifier labels with classifier function, compares pet labels to 
@@ -59,7 +61,7 @@ def classify_images(images_dir, results_dic, model):
                 --- where index 1 & index 2 are added by this function ---
                   NEW - index 1 = classifier label (string)
                   NEW - index 2 = 1/0 (int)  where 1 = match between pet image
-                    and classifer labels and 0 = no match between labels
+                    and classifier labels and 0 = no match between labels
       model - Indicates which CNN model architecture will be used by the 
               classifier function to classify the pet images,
               values must be either: resnet alexnet vgg (string)
@@ -71,8 +73,12 @@ def classify_images(images_dir, results_dic, model):
         if filename[0] != '.':
             image_path = path.join(images_dir, filename)
             image_classification = classifier(image_path, model)
-            image_classification = image_classification.lower().strip()
-            if results_dic[filename][0]  == image_classification:
+            if ',' in image_classification:
+                # add the account for multiple dog names
+                image_classification = image_classification.split(',')[0].lower().strip()
+            else:
+                image_classification = image_classification.lower().strip()
+            if results_dic[filename][0] == image_classification:
                 match = 1
             else:
                 match = 0
